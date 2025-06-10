2b5b9eab9b0e79ecf65ada4d50b57133d1ccfdfd
mutuma2011 <mutuma2011@gmail.com>
2025-06-10 09:51:02 +0300

    Move files to correct src directory structure

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,1 @@
+export const Button = ({ children, className }: any) => <button className={`py-2 px-4 rounded ${className}`}>{children}</button>;
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,2 @@
-
 export const Card = ({ children }: any) => <div className="shadow rounded bg-white">{children}</div>;
 export const CardContent = ({ children }: any) => <div className="p-4">{children}</div>;
